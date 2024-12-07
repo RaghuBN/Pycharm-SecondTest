b05a48a8e835983d86fea15c498e38f4c2fce3ce
RaghuBN <raghu.astepahead@gmail.com>
2024-12-08 00:03:04 +0530

    first commit

---
+++ Chrome Browser Launch.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://auth.hollandandbarrett.com/u/login")
+driver.maximize_window()
+time.sleep(8)
+
+actual_title = driver.title
+expect_title = "Sign in - to your account, for the best experience"
+
+if actual_title == expect_title:
+    print("Login is Successful ...... Well done Python")
+else:
+    print("Login UnSuccessful  ...... Very good my boy!")
